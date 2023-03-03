168346a12f1e8855bde8ab7e552efc9722363442
Riddh Bhattar <riddhbhattar@gmail.com>
2023-03-02 23:13:05 -0600

    Initial Commit- Downloading data

---
+++ Load_data.py
@@ -0,0 +1,14 @@
+from urllib.request import urlretrieve
+import os
+import zipfile
+import kaggle
+import opendatasets as od
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+
+dataset_url = 'https://github.com/JovianML/opendatasets/raw/master/data/house-prices-advanced-regression-techniques.zip'
+
+urlretrieve(dataset_url, 'house_prices.zip')
+
+with zipfile.ZipFile('house_prices.zip') as f:
+    f.extractall(path='house_prices')
