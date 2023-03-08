3ec442958852b8fe11828c1ff506a5973e28aca5
Riddh Bhattar <riddhbhattar@gmail.com>
2023-03-08 09:13:31 -0600

    Final Model Creation

--- house_prices/Final.py
+++ house_prices/Final.py
@@ -19,5 +19,16 @@ from sklearn.preprocessing import PolynomialFeatures
 
 train_inputs = pd.read_csv('trainv2.csv')
 test_inputs = pd.read_csv('testv2.csv')
-targets = pd.read_csv('train.csv')
-targets = targets['SalePrice']
+train_targets = pd.read_csv('train.csv')
+train_targets = train_targets['SalePrice']
+
+def evaluation(targets, predictions):
+    mae= mean_absolute_error(targets, predictions)
+    mse= mean_squared_error(targets, predictions)
+    mxe= max_error(targets, predictions)
+    r2s = r2_score(targets, predictions)
+    mape = mean_absolute_percentage_error(targets, predictions)
+    return mae, mse, mxe, r2s, mape
+
+train_targets.drop(index=5, inplace=True)
+train_inputs.drop(index=5, inplace=True)
